70a5964ea5915bbfaa3f518611b5a88c1c033db0
Russell Brian Fulache Dugaduga Jale Palma <rpalm@Russells-MacBook-Pro.local>
2024-02-08 01:29:47 -0800

    Initial launch of NexusConjure: Unleashing a new era of B2B2B digital transformation tools. 🚀 #FirstCommit

---
+++ .history/src/lib/utils_20240208001305.ts
@@ -0,0 +1,15 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+export function getStripeOAuthLink(
+	accountType: "agency" | "subaccount",
+	state: string
+) {
+	return `https://connect.stripe.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_STRIPE_CLIENT_ID}&scope=read_write&redirect_uri=${process.env.NEXT_PUBLIC_URL}${accountType}&state=${state}`;
+
+	//   https://connect.stripe.com/oauth/authorize?response_type=code&client_id=ca_PW6xzVJhxhSctp7oUPXOwIMVr9Ge0kde&scope=read_write
+}
