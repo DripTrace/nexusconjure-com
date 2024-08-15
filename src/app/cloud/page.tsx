bf90c02d3ce499da7b6303e20ddbedbb68f1cd22
DripTrace <rpalm@driptrace.io>
2024-08-15 09:38:38 -0700

    use client for cloud page

--- src/app/cloud/page.tsx
+++ src/app/cloud/page.tsx
@@ -1,10 +1,14 @@
+"use client";
+
 import View from "@/components/cloud/View";
 import { parse } from "flatted";
 
 const CloudPage = async () => {
     let initialData = null;
     try {
-        const response = await fetch(`/api/cloud/view/cloud-view`);
+        const response = await fetch(
+            `${process.env.NEXT_PUBLIC_URL}api/cloud/view/cloud-view`
+        );
         const result = await response.json();
         if (response.ok) {
             initialData = parse(result.data);
